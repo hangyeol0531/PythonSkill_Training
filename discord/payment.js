bf8170d34f09bbc2920cfef3201ed5aeb28c20bd
hangyeol0531 <41174265+hangyeol0531@users.noreply.github.com>
2019-12-28 17:40:23 +0900

    complete
    123123

--- discord/payment.js
+++ discord/payment.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
-var bodyParser = require('body-parser');
-var mysql = require('mysql');
-var sendperson, getperson, sendprice;
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const mysqlsync = require('sync-mysql');
+var sendperson, getperson, sendprice, gethavemoney;
+var sendhavemoney, sendprice;
 var db = mysql.createConnection({
     host: "localhost",
     database: "moodpayment",
     user: "root",
     password: "0000"
 })
+
+var syncdb = new mysqlsync({
+    host: "localhost",
+    database: "moodpayment",
+    user: "root",
+    password: "0000"
+})
+
 db.connect((error) =>{
     if(error){
         throw(error);
@@ -40,7 +50,6 @@ app.post('/', (req, res) =>{
                 }
                 console.log("삽입");
                 res.send('0');
-                db.end();
             });
         }
         else{
@@ -48,7 +57,6 @@ app.post('/', (req, res) =>{
             usermoney = docs[0].money;
             res.send(usermoney);
         }
-        db.end();
     });      
 });
 
@@ -59,48 +67,72 @@ app.post('/pay', (req, res) =>{
     sendperson = req.body.sendperson;
     getperson = req.body.getperson;
     sendprice = Number(req.body.sendprice);
+    
+    // db.query(`SELECT money From user Where id = ${sendperson}`, function(err, docs){
+    //     if(err){
+    //         throw err;
+    //     }
+    //     sendhavemoney = Number(docs[0].money);
+    //     console.log("보내는친구 돈가져올게 :", sendhavemoney );
+    // });
 
-    db.query(`INSERT INTO user (id, money) VALUES(${sendperson}, 0)`, function(err, docs){
-        if(err){
-            throw err;
-        }
-        console.log("보내는친구 돈가져올게");
-        sendhavemoney = Number(docs[0].money);
-        db.end();
-    });
+    let docs = syncdb.query(`SELECT money From user Where id = ${sendperson}`);
+    console.log("테스트:", docs);
+    sendhavemoney = Number(docs[0].money);
+    console.log("보내는친구 돈가져올게 :", sendhavemoney );
 
-    db.query(`INSERT INTO user (id, money) VALUES(${getperson}, 0)`, function(err, docs){
-        if(err){
-            throw err;
-        }
-        sendhavemoney = Number(docs[0].money);   
-        console.log("받는친구 돈가져올게");
-        if(JSON.stringify(docs) == '[]'){
-            console.log(typeof(JSON.stringify(docs)));
-            console.log("친구가 없나?");
-            res.end("친구 정보 없어여");
-        }
-    });
+    
+    // db.query(`SELECT money From user Where id =  ${getperson}`, function(err, docs){
+    //     if(err){
+    //         throw err;
+    //     }
+    //     gethavemoney = Number(docs[0].money);   
+    //     console.log("받는친구 돈가져올게 : ", gethavemoney);
+    //     console.log(typeof(gethavemoney))
+    //     if(JSON.stringify(docs) == '[]'){
+    //         console.log(typeof(JSON.stringify(docs)));
+    //         console.log("친구가 없나?");
+    //         res.end("친구 정보 없어여");
+    //     }
+    // });
 
-    if(sendhavemoney - sendprice) res.end("잔액 부족인디?");
+    docs = syncdb.query(`SELECT money From user Where id = ${getperson}`);
+    console.log("docs : ", docs);
+    if(JSON.stringify(docs) == '[]'){
+        console.log(typeof(JSON.stringify(docs)));
+        console.log("친구가 없나?");
+        res.end("친구 정보 없어여");
+        return ;    
+    }else{
+        gethavemoney = Number(docs[0].money);   
+        console.log("받는친구 돈가져올게 : ", gethavemoney);
+        console.log(typeof(gethavemoney))
+    }
+
+    if(sendhavemoney - sendprice < 0) res.end("잔액 부족인디?");
     else{
-        db.query(`UPDATE user SET money = ${sendhavemoney - sendprice} WHERE ${sendperson}`, function(err, docs){
-            if(err){
-                throw err;
-            }
-            console.log("주는 친구 돈 가져갈게");
-            db.end();
-        });
+        console.log("확인");
+        console.log("sendhavemoney :", sendhavemoney);
+        console.log("gethavemoney : ", gethavemoney);
+        console.log("sendprice: ", sendprice);
 
-        db.query(`UPDATE user SET money = ${sendhavemoney + sendprice} WHERE ${getperson}`, function(err, docs){
+        db.query(`UPDATE user SET money = ${gethavemoney + sendprice} WHERE id = ${getperson}`, function(err, docs){
             if(err){
                 throw err;
             }
             console.log("받는 친구 돈 줄게");  
-            db.end();
         });
+
+        db.query(`UPDATE user SET money = ${sendhavemoney - sendprice} WHERE id = ${sendperson}`, function(err, docs){
+            if(err){
+                throw err;
+            }
+            console.log("주는 친구 돈 가져갈게");
+        });
+        res.end(`${sendhavemoney - sendprice}`);
     }
     console.log("처리 완료");
+    
 });
 
 app.listen(3000, () =>{

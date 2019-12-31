70d47d9142fb22645b3e54e76ba2cc24b3d970ea
hangyeol0531 <41174265+hangyeol0531@users.noreply.github.com>
2019-12-31 14:18:18 +0900

    33

--- discord/mood_coin.py
+++ discord/mood_coin.py
@@ -4,11 +4,11 @@ import requests
 import json
 
 
-url = "http://localhost:8080"
+url = "http://10.120.72.127:8080/"
 client = discord.Client()
-name = "성범이이"
+name = "한결"
 # 생성된 토큰을 입력해준다.
-token = "NjU3MDI1MDY4OTI0MjcyNjUx.Xfrfrg.BMv0UsWn5_EVB-WI97iYWg_ZwJo"
+token = "NjU3MDI1MDY4OTI0MjcyNjUx.XglJVA.jjmNeQyFfl1tp5kqt1Jk5eU7sUM"
 
 # 봇이 구동되었을 때 보여지는 코드
 @client.event
@@ -21,23 +21,23 @@ async def on_ready():
 # 봇이 특정 메세지를 받고 인식하는 코드
 @client.event
 async def on_message(message):
-    id = message.author.id
-
+    userid = message.author.id
+    username = message.author.name
+    channel = message.channel
     # 메세지를 보낸 사람이 봇일 경우 무시한다
     if message.author.bot:
         return None
-
-    if message.content.startswith('!안녕'):
-        channel = message.channel
-        await channel.send('반가워!')
-
-    username = message.author.name
-    print(username)
-    if name != username:
-        if message.content:
-            channel = message.channel
-            await channel.send(message.content)
-            data = {'msg': message.content}
-            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-            r = requests.post(url, data=json.dumps(data), headers=headers) 
+    
+    if message.content:
+        #await channel.send(message.content)
+        data = {'id' : userid,
+                'name': username,
+                'talk': message.content
+                }       
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        #await channel.send(data)
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        #await channel.send(r)
+        
+            
 client.run(token)
--- discord/payment.js
+++ discord/payment.js
@@ -3,7 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const mysqlsync = require('sync-mysql');
-var sendperson, getperson, sendprice, gethavemoney;
+const delay = require('delay');
+
+var sendperson, getperson, sendprice, gethavemoney, usermoney;
 var sendhavemoney, sendprice;
 var db = mysql.createConnection({
     host: "localhost",
@@ -31,8 +33,8 @@ db.connect((error) =>{
 app.use(bodyParser.json())
 
 app.post('/', (req, res) =>{
-    var usermoney = 0;
     console.log("머기")
+    valueclear()
     console.log(req.body);
     console.log(typeof(req.body))
     sendperson = req.body.sendperson;
@@ -41,6 +43,7 @@ app.post('/', (req, res) =>{
         if(err){
             throw err;
         }
+        usermoney = 0;
         if(JSON.stringify(docs) == '[]'){
             console.log(typeof(JSON.stringify(docs)));
             console.log("값이 없나?");
@@ -55,47 +58,27 @@ app.post('/', (req, res) =>{
         else{
             console.log("돈확인:", docs);
             usermoney = docs[0].money;
-            res.send(usermoney);
+            console.log("들어갔다.");
+            console.log("money type", typeof(usermoney))
+            res.send(usermoney.toString());
         }
     });      
 });
 
 app.post('/pay', (req, res) =>{
     console.log("머기중");
+    valueclear()
     console.log(req.body);
     console.log(typeof(req.body))
     sendperson = req.body.sendperson;
     getperson = req.body.getperson;
     sendprice = Number(req.body.sendprice);
     
-    // db.query(`SELECT money From user Where id = ${sendperson}`, function(err, docs){
-    //     if(err){
-    //         throw err;
-    //     }
-    //     sendhavemoney = Number(docs[0].money);
-    //     console.log("보내는친구 돈가져올게 :", sendhavemoney );
-    // });
-
     let docs = syncdb.query(`SELECT money From user Where id = ${sendperson}`);
     console.log("테스트:", docs);
     sendhavemoney = Number(docs[0].money);
     console.log("보내는친구 돈가져올게 :", sendhavemoney );
-
-    
-    // db.query(`SELECT money From user Where id =  ${getperson}`, function(err, docs){
-    //     if(err){
-    //         throw err;
-    //     }
-    //     gethavemoney = Number(docs[0].money);   
-    //     console.log("받는친구 돈가져올게 : ", gethavemoney);
-    //     console.log(typeof(gethavemoney))
-    //     if(JSON.stringify(docs) == '[]'){
-    //         console.log(typeof(JSON.stringify(docs)));
-    //         console.log("친구가 없나?");
-    //         res.end("친구 정보 없어여");
-    //     }
-    // });
-
+ 
     docs = syncdb.query(`SELECT money From user Where id = ${getperson}`);
     console.log("docs : ", docs);
     if(JSON.stringify(docs) == '[]'){
@@ -135,10 +118,87 @@ app.post('/pay', (req, res) =>{
     
 });
 
+app.post('/mywallet', (req, res) =>{
+    console.log("mywallet 접속");
+    valueclear()
+    sendperson = req.body.sendperson;
+    db.query(`SELECT money FROM user WHERE id = ${sendperson}`, function(err, docs){
+        if(err){
+            throw err;
+        }
+        if(JSON.stringify(docs) == '[]'){
+            console.log(typeof(JSON.stringify(docs)));
+            console.log("값이 없나?");
+            db.query(`INSERT INTO user (id, money) VALUES(${sendperson}, 0)`, function(err, result, fields){
+                if(err){
+                    throw err;
+                }
+                console.log("삽입");
+                res.send('0');
+            });
+        }
+        else{
+            console.log("돈확인:", docs);
+            usermoney = docs[0].money;
+            console.log("money tpye", typeof(usermoney))
+            res.send(usermoney.toString());
+        }
+    });    
+});
+
+app.post('/statemoney', (req, res) =>{
+    console.log("statemoney 접속");
+    valueclear()
+    sendperson = req.body.sendperson;
+    var addprice = Number(req.body.addprice);
+    console.log(typeof(sendperson), "  ", typeof(addprice))
+    console.log(sendperson, addprice);
+    if(addprice == null) addprice = 0;
+    db.query(`SELECT money FROM user WHERE id = ${sendperson}`, function(err, docs){
+        if(err){
+            throw err;
+        }
+        if(JSON.stringify(docs) == '[]'){
+            console.log(typeof(JSON.stringify(docs)));
+            console.log("값이 없나?");
+            usermoney = 0;
+            db.query(`INSERT INTO user (id, money) VALUES(${sendperson}, 0)`, function(err, result, fields){
+                if(err){
+                    throw err;
+                }
+                console.log("삽입");
+                res.send('0');
+                return ;
+            });
+        }
+        else{
+            console.log("돈확인:", docs);
+            usermoney = docs[0].money;
+            console.log("money tpye", typeof(usermoney))
+        }
+
+        console.log(`usermoney : ${usermoney}`);
+        console.log(`addprice : ${addprice}`);
+        console.log(`UPDATE user SET money = ${usermoney + addprice} WHERE id = ${sendperson}`);
+        db.query(`UPDATE user SET money = ${usermoney + addprice} WHERE id = ${sendperson}`, function(err, docs){
+            if(err){
+                throw err;
+            }
+            console.log("돈 적립");  
+            res.send((usermoney + addprice).toString());
+        });
+    });    
+});
+
 app.listen(3000, () =>{
     console.log("서버 포트 3000번으로 오픈했습니다");
 });
-
-// object.put("sendperson", sendperson);
-// object.put("getperson", getperson );
-// object.put("sendprice", sendprice);
+ 
+function valueclear(){
+    console.log("valueclear")
+    sendperson = 0;
+    getperson = 0; 
+    sendprice = 0;
+    gethavemoney = 0;
+    usermoney = 0;
+}

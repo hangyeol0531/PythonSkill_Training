e271014e394900c377f3ed719323497954d90da3
hangyeol0531 <jhan0531@naver.com>
2020-11-03 15:40:33 +0900

    flask_connecting arduino

--- flask/RPAQ1server.py
+++ flask/RPAQ1server.py
@@ -3,21 +3,52 @@ from pyfirmata import Arduino, util
 import serial
 
 app = Flask(__name__)
-ser = serial.serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyACM1', 9600)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "connecting"
 
-@app.route("/on")
-def on():
+@app.route("/red_on")
+def on1():
     ser.write(b'1')
     return "on 접속"
 
-@app.route("/off")
-def off():
-    ser.write(b'0')
+@app.route("/red_off")
+def off1():
+    ser.write(b'2')
     return "off 접속"
+##########################
+@app.route("/blue_on")
+def on2():
+    ser.write(b'3')
+    return "on 접속"
+
+@app.route("/blue_off")
+def off2():
+    ser.write(b'4')
+    return "off 접속"
+##########################
+@app.route("/green_on")
+def on3():
+    ser.write(b'5')
+    return "on 접속"
+
+@app.route("/green_off")
+def off3():
+    ser.write(b'6')
+    return "off 접속"
+##########################
+@app.route("/all_on")
+def on4():
+    ser.write(b'7')
+    return "on 접속"
+
+@app.route("/all_off")
+def off4():
+    ser.write(b'8')
+    return "off 접속"
+##########################
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = 8080)
